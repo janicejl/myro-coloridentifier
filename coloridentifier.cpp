b1a750a7ccf438e903cf6abda9c3c2f127505018
Janice Leung <jjw1707@gmail.com>
2013-04-03 00:19:25 -0700

    working version of color identifier. finds the average hex

--- coloridentifier.cpp
+++ coloridentifier.cpp
@@ -18,6 +18,11 @@ void readColorListOne(map<string, string> &list);
 void readColorListTwo(map<string, string> &list);
 void readColorListThree(map<string, string> &list);
 void readColorListFour(map<string, string> &list);
+string findClosest(string original, map<string, string> list);
+int getR(string hex);
+int getG(string hex);
+int getB(string hex);
+int hexToDec(char c);
 
 
 int main(int argc, char ** argv) {
@@ -31,17 +36,21 @@ int main(int argc, char ** argv) {
 	readColorListOne(list);
 	cout<<list.size()<<endl;
 
+	// string asdf = findClosest("#1ABF29", list);
+
+	// cout<<asdf<<" = "<<list[asdf]<<endl;
+
 	connect();
 	cout << "Waiting..." << endl;
 	wait(1);
 
-	//robot.setWhiteBalance(0);
-	//robot.darkenCamera(0);
-
 	while(true) {
 		string hex = analyzePicture();
 
-		cout<<hex<<" = "<<list[hex]<<endl;
+		string closestHex = findClosest(hex, list);
+
+		cout<<endl;
+		cout<<list[closestHex]<<endl;
 	}	
 	
 	cout << "Waiting..." << endl;
@@ -224,4 +233,109 @@ void readColorListFour(map<string, string> &list) {
 	}
 
 	in.close();	
+}
+
+
+string findClosest(string original, map<string, string> list) {
+	const int R = getR(original);
+	const int G = getG(original);
+	const int B = getB(original);
+
+	// cout<<R<<" "<<G<<" "<<B<<endl;
+
+	string key, value;
+
+	string nearest = "";
+
+	double difference = 500.0;
+
+	typedef map<string, string>::iterator it_type;
+	for(it_type iterator = list.begin(); iterator != list.end(); iterator++) {
+		int keyR = 0, keyG = 0, keyB = 0;
+
+	    key = iterator->first;
+	    value = iterator->second;
+
+	    keyR = getR(key);
+	    keyG = getG(key);
+	    keyB = getB(key);
+
+	    if ((R == keyR) && (G == keyG) && (B == keyB)) {
+	    	return original;
+	    }
+
+	    if (((keyR == 0) && (keyG == 0) && (keyB == 0)) || ((keyR == 255) && (keyG == 255) && (keyB == 255))) {
+	    	continue;
+	    }
+
+	    double distance = sqrt((R - keyR)*(R - keyR) + (G - keyG)*(G - keyG) + (B - keyB)*(B - keyB));
+
+	    // cout<<distance<<endl;
+
+	    if (distance < difference) {
+	    	difference = distance;
+	    	nearest = key;
+	    	// cout<<"New nearest: "<<nearest<<endl;
+	    }
+   
+	}
+	// cout<<R<<" "<<G<<" "<<B<<endl;
+	// cout<<original<<" "<<nearest<<endl;
+	// cout<<difference<<endl;
+	return nearest;
+}
+
+int getR(string hex) {
+	int r1 = hexToDec((char)hex[1])*16;
+	int r2 = hexToDec((char)hex[2]);
+
+	return r1+r2;
+}
+int getG(string hex) {
+	int g1 = hexToDec((char)hex[3])*16;
+	int g2 = hexToDec((char)hex[4]);
+
+	return g1+g2;
+}
+int getB(string hex) {
+	int b1 = hexToDec((char)hex[5])*16;
+	int b2 = hexToDec((char)hex[6]);
+
+	return b1+b2;
+}
+
+int hexToDec(char c) {
+	if (c == '0') {
+		return 0;
+	} else if (c == '1') {
+		return 1;
+	} else if (c == '2') {
+		return 2;
+	} else if (c == '3') {
+		return 3;
+	} else if (c == '4') {
+		return 4;
+	} else if (c == '5') {
+		return 5;
+	} else if (c == '6') {
+		return 6;
+	} else if (c == '7') {
+		return 7;
+	} else if (c == '8') {
+		return 8;
+	} else if (c == '9') {
+		return 9;
+	} else if (c == 'A' || c == 'a') {
+		return 10;
+	} else if (c == 'B' || c == 'b') {
+		return 11;
+	} else if (c == 'C' || c == 'c') {
+		return 12;
+	} else if (c == 'D' || c == 'd') {
+		return 13;
+	} else if (c == 'E' || c == 'e') {
+		return 14;
+	} else if (c == 'F' || c == 'f') {
+		return 15;
+	} 
 }
